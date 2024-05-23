3af7ddfc3a4787c39a7a5a1fb22b412ab69d9749
Phoenix <phoenixreid@mail.ru>
2024-05-23 17:06:23 +0300

    FirstCommit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13676;
+        int miles = 20;
+
+        int bonus = price/miles;
+        System.out.println("Количество бонусов: " +bonus);
+    }
+}
